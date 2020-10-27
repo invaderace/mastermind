85c6cfae49e28b70f47fbb74586cb9e72f785cc7
Michael Mattson <michael.p.mattson@gmail.com>
2020-10-27 16:01:04 -0600

    added some comments, class Game

--- mastermind.rb
+++ mastermind.rb
@@ -19,22 +19,26 @@ class Row
   end
 end
 
-# this is for the colored pegs used in gameplay
+# this is for the colored pegs used in gameplay. holds 4 pegs. 12 rows total.
 class CodeRow < Row
 end
 
-# this class is the shielded row that stores the answer
+# this class is the shielded row that stores the answer. only 1 row.
 class ShieldRow < CodeRow
 end
 
-# this is for the red and white pegs used to show the current guess accuracy.
+# this is for the red and white pegs used to show the current guess accuracy. each holds 4 pegs just like row. 12 rows total.
 class KeyRow < Row
 end
 
-# the score
+# the score - there will be two of these (white and red), with cells up to 30
 class Score
 end
 
 # the Board
 class Board
+end
+
+# the game
+class Game
 end
