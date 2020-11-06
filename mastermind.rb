5269cf2b4a08ce0504826cd0cd49c40b46fa1c81
Michael Mattson <michael.p.mattson@gmail.com>
2020-11-06 10:20:13 -0700

    added Score total (a number), and Score.board (array) to visually display score, commented some unused things out

--- mastermind.rb
+++ mastermind.rb
@@ -33,11 +33,20 @@ end
 
 # the score - there will be two of these (white and red), with cells up to 30
 class Score
+  attr_accessor :total, :board
+
+  def initialize
+    @total = 0
+    @board = []
+    30.times do
+      @board.push('o')
+    end
+  end
 end
 
 # the Board
 class Board
-  attr_accessor :board, :shield_row, :rows, :key_rows, :score
+  attr_accessor :board, :shield_row, :rows, :key_rows, :player1_score, :player2_score, :scoreboard
 
   def initialize
     @board = board
@@ -48,19 +57,29 @@ class Board
       @rows.push(Row.new)
       @key_rows.push(KeyRow.new)
     end
-    @score = []
+    @player1_score = Score.new
+    @player2_score = Score.new
+    @scoreboard = []
   end
 
-  def row_colors(i) #allows to access the row of colors. not sure how to use this just yet.
-    p @rows[i].row[0].color
-    p @rows[i].row[1].color
-    p @rows[i].row[2].color
-    p @rows[i].row[3].color
+  def fill_scoreboard
+    30.times { |i| 
+      @scoreboard.push([@player1_score.board[i], @player2_score.board[i]])
+      i += 1
+    }
   end
 
-  def view_current
-    puts @board[11]
-  end
+  # allows to access the row of colors. not sure how to use this just yet.
+  # def row_colors(i)
+  #   p @rows[i].row[0].color
+  #   p @rows[i].row[1].color
+  #   p @rows[i].row[2].color
+  #   p @rows[i].row[3].color
+  # end
+
+  # def view_current
+  #   puts @board[11]
+  # end
 end
 
 # the game
