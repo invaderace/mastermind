eeaf39494aefb56d4f12229d06384f6012002be2
Michael Mattson <michael.p.mattson@gmail.com>
2020-11-10 12:35:27 -0700

    added attr_accessors for shield, decoding, and key rows. changed names of make functions. added make decoding/key rows method and combined.

--- mastermind.rb
+++ mastermind.rb
@@ -5,27 +5,28 @@
 # the same row function. decode rows probably can be stored in a hash for easy
 # access.
 class Board
+  attr_accessor :shield_row, :decoding_rows, :key_rows
+
   def initialize
-    @shield_row = shield_row
-    @decode_rows = decode_rows
-    @key_rows = key_rows
+    @shield_row = make_shield_row
+    @decoding_rows = make_decoding_or_key_rows
+    @key_rows = make_decoding_or_key_rows
   end
 
-  def row
-    []
+  def make_row
+    Array.new(4, '_')
   end
 
-  def shield_row
-    row
+  def make_shield_row
+    make_row
   end
 
   # this should hold 12 key/value combos. key: 1-12. value: array of entries.
-  def decode_rows
-    {}
-  end
-
-  def key_rows
-    {}
+  def make_decoding_or_key_rows
+    decoding_rows = {}
+    i = 0
+    decoding_rows[i += 1] = make_row until i == 12
+    decoding_rows
   end
 end
 
